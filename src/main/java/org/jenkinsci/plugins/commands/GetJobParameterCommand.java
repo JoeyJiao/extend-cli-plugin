fdeec6d1699b1ecb1af0325281993743f5e01ad1
Joey Jiao <joey.jiaojg@gmail.com>
2013-06-06 13:10:08 +0800

    Init commit
    Add two CLI commands
    - get-job-parameter <job> <parameter>
    - get-job-parameters <job>
    
    Signed-off-by: Joey Jiao <joey.jiaojg@gmail.com>

---
+++ src/main/java/org/jenkinsci/plugins/commands/GetJobParameterCommand.java
@@ -0,0 +1,34 @@
+package org.jenkinsci.plugins.commands;
+
+import hudson.Extension;
+import hudson.cli.CLICommand;
+import hudson.model.AbstractProject;
+import hudson.model.Item;
+import hudson.model.ParametersDefinitionProperty;
+import org.kohsuke.args4j.Argument;
+
+@Extension
+public class GetJobParameterCommand extends CLICommand{
+	@Argument(metaVar = "JOB", usage = "Name of the job", required = true)
+	public AbstractProject<?,?> job;
+	@Argument(metaVar = "PARAMETER", usage = "Name of the parameter", index = 1, required = true)
+	public String parameter;
+
+	@Override
+	public String getShortDescription() {
+		return Messages.GetJobParameter_ShortDescription();
+	}
+
+	@Override
+	protected int run() throws Exception {
+		job.checkPermission(Item.EXTENDED_READ);
+		try {
+			ParametersDefinitionProperty property = job.getProperty(ParametersDefinitionProperty.class);
+			stdout.println(property.getParameterDefinition(parameter).getDefaultParameterValue());
+		} catch (NullPointerException e) {
+			stderr.println("No such parameter");
+			return 1;
+		}
+		return 0;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+}
---
+++ src/main/java/org/jenkinsci/plugins/commands/GetJobParametersCommand.java
@@ -0,0 +1,37 @@
+package org.jenkinsci.plugins.commands;
+
+import hudson.Extension;
+import hudson.cli.CLICommand;
+import hudson.model.AbstractProject;
+import hudson.model.Item;
+import hudson.model.ParameterDefinition;
+import hudson.model.ParametersDefinitionProperty;
+import org.kohsuke.args4j.Argument;
+
+@Extension
+public class GetJobParametersCommand extends CLICommand{
+	@Argument(metaVar = "JOB", usage = "Name of the job", required = true)
+	public AbstractProject<?,?> job;
+
+	@Override
+	public String getShortDescription() {
+		return Messages.GetJobParameters_ShortDescription();
+	}
+
+	@Override
+	protected int run() throws Exception {
+		job.checkPermission(Item.EXTENDED_READ);
+		try {
+			ParametersDefinitionProperty property = job.getProperty(ParametersDefinitionProperty.class);
+			if (property != null && property.getParameterDefinitions() !=null) {
+				for (ParameterDefinition p : property.getParameterDefinitions()) {
+					stdout.println(p.getDefaultParameterValue());
+				}
+			}
+		} catch (NullPointerException e) {
+			stderr.println("No parameters");
+			return 1;
+		}
+		return 0;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+}
